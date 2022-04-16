ac715cd592e613fa66d3ea2e3a7f0bc6c9a5a923
HugoRLA <hugoricardola@gmail.com>
2022-04-16 14:43:43 +0100

    add id for projects and tasks to mock data

--- backend/data/projectsMock.js
+++ backend/data/projectsMock.js
@@ -1,19 +1,23 @@
 module.exports =
 [
         {
+            "id":"0",
             "name": "project1",
             "tasks": []
         },
         {
+            "id":"1",
             "name": "project2",
             "tasks": [
                 {
+                    "id":"0",
                     "name": "task1-project2",
                     "desc": "desc-task1-project2",
                     "created_date": "2012-04-23T18:25:43.511Z",
                     "finished_date": "2012-04-25T18:25:43.511Z"
                 },
                 {
+                    "id":"1",
                     "name": "task2-project2",
                     "desc": "desc-task2-project2",
                     "created_date": "2012-04-26T18:25:43.511Z",
