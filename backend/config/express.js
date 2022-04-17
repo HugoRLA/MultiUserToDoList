87ae2f18981862852adc44f447681f8a7d3a2f39
HugoRLA <hugoricardola@gmail.com>
2022-04-17 11:11:53 +0100

    accepted all origns

--- backend/config/express.js
+++ backend/config/express.js
@@ -1,6 +1,7 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 const config     = require('config');
+const cors = require('cors');
 
 module.exports = () => {
   const app = express();
@@ -8,6 +9,10 @@ module.exports = () => {
   app.set('port', process.env.PORT || config.get('server.port'));
 
   app.use(bodyParser.json());
+
+  app.use(cors({
+    origin: '*'
+  }));
 
   app.use('/projects', require('../routers/projects'));
 
